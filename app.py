32cbb7da363867a3f686aeaf104a207c6d56786f
MucTepKOT <53336047+MucTepKOT@users.noreply.github.com>
2020-01-29 08:13:08 +0300

    client can upload .xlsx files on server

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 import json
+import os
 import flask
+from werkzeug.utils import secure_filename
+from openpyxl import load_workbook
 import convert_func
 import do_xml
 
+
+UPLOAD_FOLDER = f'{os.getcwd()}\\uploads'
+ALLOWED_EXTENSIONS = set(['xlsx'])
+
 app = flask.Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024
 
 def response(code, data):
     return flask.Response(
@@ -35,36 +44,51 @@ def converter():
         output_2 = 'Введите продуктовый код'
     return flask.render_template('converter.html', product_code=output_1, id_code=output_2)
 
-# @app.route('/id_code', methods=['GET'])
-# def get_id_code(product_code):
-#     product_code = flask.request.args.get('product_code')
-#     id_code = convert_func.convert_to_IC(product_code)
-#     return response(200, {"id_code": id_code})
-
-
-# @app.route('/product_code', methods=['GET'])
-# def get_product_code(id_code):
-#     id_code = flask.request.args.get('id_code')
-#     product_code = convert_func.convert_to_PC(id_code)
-#     return response(200, {"product_code": product_code})
-
-
 @app.route('/order_codes', methods=['GET'])
 def order_codes():
     return flask.render_template('order_codes.html')
 
-@app.route('/uploads', methods=['POST'])
-def download():
+@app.route('/user_input', methods=['POST'])
+def user_input():
     order_dict = flask.request.form.to_dict(flat=False)
     print(order_dict)
     do_xml.xml_builder(order_dict)
-    path = "answer.xml"
+    path = "answer.zip"
     return flask.send_file(path, as_attachment=True)
 
-    # order_dict = flask.request.get_json()
-    # do_xml.xml_builder(order_dict)
-    # path = "answer.xml"
-    # return flask.send_file(path, as_attachment=True)
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def parse_xlsx(filepath):
+    wb = load_workbook(filepath)
+    ws = wb['Данные']
+    all_gtin = ws['B']
+    all_gtin_list = []
+    all_quantity = ws['S']
+    all_quantity_list = []
+    for row in all_gtin[6:-1]:
+        all_gtin_list.append(str(row.value))
+    for row in all_quantity[6:-1]:
+        all_quantity_list.append(str(row.value))
+    return [all_gtin_list, all_quantity_list]
+
+@app.route('/user_file', methods=['POST'])
+def user_file():
+    order_dict = flask.request.form.to_dict(flat=False)
+    print(order_dict)
+    file = flask.request.files['file']
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)
+        gtin_and_quantity_lists = parse_xlsx(filepath)
+        order_dict['product_code'] = gtin_and_quantity_lists[0]
+        order_dict['quantity'] = gtin_and_quantity_lists[1]
+        do_xml.xml_builder(order_dict)
+        path = "answer.zip"
+        return flask.send_file(path, as_attachment=True)
+    return '404'
 
 
 if __name__ == '__main__':
--- do_xml.py
+++ do_xml.py
@@ -1,5 +1,10 @@
 import xml.etree.ElementTree as ET
+import os
+import shutil
 
+def separate_list(lst, n):
+    '''разделяет список на двумерный список в каждом елементе которого n элементов ([1,2,3,4] --> [[1,2],[3,4]] при n=2) '''
+    return [lst[i:i + n] for i in range(0, len(lst), n)]
 
 def xml_builder(order_dict):
     elem = {"xmlns": "urn:oms.order", "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance", "xsi:schemaLocation": "urn:oms.order order-v5.0.xsd"}
@@ -20,19 +25,40 @@ def xml_builder(order_dict):
     contractDate.text = order_dict['contractDate'][0]
     products = ET.SubElement(shoes, 'products')
 
-    codes_and_quantity = dict(zip(order_dict['product_code'], order_dict['quantity']))
+    # если ты решил, что выше полная дичь написана, спешу тебя огорчить, самый пиздец ниже
+    # задача такая: при формировании xml, в products может быть не > 10 product
+    # на вход подаётся xlsx, в котором мы находим пары gtin, quantity
+    # из которых составляется products
 
-    for key, value in codes_and_quantity.items():
-        product = ET.Element('product')
-        gtin = ET.SubElement(product, 'gtin')
-        gtin.text = key
-        quantity = ET.SubElement(product, 'quantity')
-        quantity.text = value
-        serialNumberType = ET.SubElement(product, 'serialNumberType')
-        serialNumberType.text = 'OPERATOR'
-        templateId = ET.SubElement(product, 'templateId')
-        templateId.text = '1'
-        products.append(product)
+    sep_product_code_list = separate_list(order_dict['product_code'], 10)
+    print(sep_product_code_list)
+    sep_quantity_list = separate_list(order_dict['quantity'], 10)
+    print(sep_quantity_list)
+    big_codes_and_quantity = zip(sep_product_code_list, sep_quantity_list)
 
-    tree = ET.ElementTree(root)
-    tree.write('answer.xml', encoding="utf-8")
+    counter = 0
+    filepath = f'{os.getcwd()}\\xml_dir'
+    print(filepath)
+    
+
+    for product_code, quantity in big_codes_and_quantity:
+        small_codes_and_quantity = dict(zip(product_code, quantity))
+        for key, value in small_codes_and_quantity.items():
+            product = ET.Element('product')
+            gtin = ET.SubElement(product, 'gtin')
+            gtin.text = key
+            quantity = ET.SubElement(product, 'quantity')
+            quantity.text = value
+            serialNumberType = ET.SubElement(product, 'serialNumberType')
+            serialNumberType.text = 'OPERATOR'
+            templateId = ET.SubElement(product, 'templateId')
+            templateId.text = '1'
+            products.append(product)
+        tree = ET.ElementTree(root)
+             
+        filename = f'answer{counter}.xml'
+        tree.write(os.path.join(filepath, filename), encoding="utf-8")
+        counter += 1
+    
+    shutil.make_archive('answer', 'zip', filepath, filepath)
+    print(f'{os.getcwd()}\\archives')
